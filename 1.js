89d1252b2cf24879686834ed3d92853d284a60c0
muktiag29 <muktiag290998@gmail.com>
2022-06-16 19:16:36 +0700

    initial commit

---
+++ 1.js
@@ -0,0 +1,9 @@
+/**
+ *  Buatlah program dimana program tersebut akan menghitung panjang string.
+ *  Jika panjang adalah bilangan genap, maka akan terprint "WOHO".
+ *  Apabila bilangan ganjil, akan terprint "OHOW".
+ *  Dan kalau diluar itu, maka akan terprint "ERROR".
+ */
+
+let string = 'abcde';
+// your code here
---
+++ 2.js
@@ -0,0 +1,10 @@
+/**
+ *  Buatlah program dimana program tersebut akan mem-filter harga ( number ).
+ *  Apabila angka yang dimasukan kurang dari 1, maka akan terprint "TIDAK VALID".
+ *  Dan apabila angka yang dimasukan lebih besar sama dengan 1 dan lebih kecil sama dengan 100000, maka   akan terprint "MURAH BANGET".
+ *  Apabila lebih dari 100000, akan terprint "MAHAL BANGET".
+
+ */
+
+let harga = 10000;
+// your code here
